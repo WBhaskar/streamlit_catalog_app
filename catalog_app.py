f57d4228732213c3c861a4b8d9179527456b4801
WBhaskar <76418847+WBhaskar@users.noreply.github.com>
2022-11-30 09:26:55 +0530

    Update catalog_app.py

--- catalog_app.py
+++ catalog_app.py
@@ -1,3 +1,16 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('Dress Catalog')
+try:
+  dress_choice = streamlit.text_input('What dress will you like to view?')
+  if not dress_choice:
+     streamlit.error("Please select a dress to get information.")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+except URLError as e:
+  streamlit.error()
